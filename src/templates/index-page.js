9bb07061ecf1e9d175cb5ee268a5747866c9dec2
Brian Clovis <bwclovis.web@gmail.com>
2019-08-30 16:55:24 -0400

    configure homepage

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -15,80 +15,52 @@ export const IndexPageTemplate = ({
   description,
   intro,
 }) => (
-  <div>
-    <div
-      className="container"
-      style={{
-        backgroundImage: `url(${
-          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`,
-        backgroundPosition: `top left`,
-        backgroundAttachment: `fixed`,
-      }}
-    >
-      
-      <div
-        style={{
-          display: 'flex',
-          height: '150px',
-          lineHeight: '1',
-          justifyContent: 'space-around',
-          alignItems: 'left',
-          flexDirection: 'column',
-        }}
-      >
-        <h1 className="">
+  <div className="container">
+    <div>
+      <div>
+        <h1 className="heading heading-h1">
           {title}
         </h1>
-        <p className="">
+        <p className="subheading-h2">
           {subheading}
         </p>
       </div>
     </div>
     <section className="section section--gradient">
-      <div className="container">
-        <div className="section">
-          <div className="columns">
-            <div className="column is-10 is-offset-1">
-              <div className="content">
-                <div className="content">
-                  <div className="tile">
-                    <h1 className="title">{mainpitch.title}</h1>
-                  </div>
-                  <div className="tile">
-                    <h3 className="subtitle">{mainpitch.description}</h3>
-                  </div>
-                </div>
-                <div className="columns">
-                  <div className="column is-12">
-                    <h3 className="has-text-weight-semibold is-size-2">
-                      {heading}
-                    </h3>
-                    <p>{description}</p>
-                  </div>
-                </div>
-                <Features gridItems={intro.blurbs} />
-                <div className="columns">
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/products">
-                      See all products
-                    </Link>
-                  </div>
-                </div>
-                <div className="column is-12">
-                  <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
-                  </h3>
-                  <BlogRoll />
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/blog">
-                      Read more
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+
+      <div className="content">
+        <div className="tile">
+          <h2 className="title">{mainpitch.title}</h2>
+        </div>
+        <div className="tile">
+          <h3 className="subtitle">{mainpitch.description}</h3>
+        </div>
+      </div>
+      
+        <div className="column is-12">
+          <h3 className="has-text-weight-semibold is-size-2">
+            {heading}
+          </h3>
+          <p>{description}</p>
+        </div>
+      
+      <Features gridItems={intro.blurbs} />
+      <div className="columns">
+        <div className="column is-12 has-text-centered">
+          <Link className="btn" to="/products">
+            See all products
+          </Link>
+        </div>
+      </div>
+      <div className="column is-12">
+        <h3 className="has-text-weight-semibold is-size-2">
+          Latest stories
+        </h3>
+        <BlogRoll />
+        <div className="column is-12 has-text-centered">
+          <Link className="btn" to="/blog">
+            Read more
+          </Link>
         </div>
       </div>
     </section>
